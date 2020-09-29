3f1a239d4ec4efc6301f8de5b56efbd21f92eac0
Syeda Najah Narjish <najah.masum@gmail.com>
2020-09-29 19:32:26 +0200

    najah's work

---
+++ src/components/Login/firebase.login.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDZa_Capb2REHdtKzqup0HbSmdmIq3s_co",
+    authDomain: "burj-al-arab-273db.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-273db.firebaseio.com",
+    projectId: "burj-al-arab-273db",
+    storageBucket: "burj-al-arab-273db.appspot.com",
+    messagingSenderId: "690990085760",
+    appId: "1:690990085760:web:d3689332947e319b57d865"
+  };
+
+  export default firebaseConfig;
